48df8eb532107e68dc6d01c47438a3c6ee27f319
thomasjmurphy <thomasjmurphy@users.noreply.github.com>
2016-06-09 12:51:13 -0400

    Update and rename Leet121.cpp to BuySell.cpp

--- BuySell.cpp
+++ BuySell.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+//LeetCode problem 121 buy sell stock 1
     int maxProfit(vector<int>& prices) 
     {
         if(prices.size() == 0){return 0;}
@@ -11,5 +13,19 @@ public:
             if(prices[i] - min > maxProfit){maxProfit = prices[i] - min;}
         }
         return maxProfit;
+    }
+};
+
+//LeetCode problem 122 buy sell stock 2
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0){return 0;}
+        int total = 0;
+        for(int i = 0;i<prices.size()-1;i++)
+        {
+            if(prices[i] < prices[i+1]){total += prices[i+1]-prices[i];}
+        }
+        return total;
     }
 };
